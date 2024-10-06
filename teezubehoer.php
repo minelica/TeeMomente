0a10f1f9380a2863fcf3f32d452e8920f66413b5
m.masinovic <masinovicminela@gmail.com>
2024-10-06 23:13:05 +0200

    teezubehoer.php fertig

--- teezubehoer.php
+++ teezubehoer.php
@@ -19,27 +19,27 @@ include('view/menue.php');
 
   <div class="container">
     <img src="img/Produkt6.jpg" alt="Produkt6">
-    <p class="beschreibungstext">Dies ist ein Beispieltext, der rechts neben dem Bild angezeigt wird. </p>
+    <p class="beschreibungstext">Nachhaltiges, handgefertigtes Teesieb aus Bambus, perfekt für losen Tee. Umweltfreundlich und langlebig. </br> Preis: 4,99 €</p>
   </div>
 
   <div class="container">
-    <p class="beschreibungstext">Dies ist ein Beispieltext, der links neben dem Bild angezeigt wird. </p>
+    <p class="beschreibungstext">Traditioneller Bambusbesen zur Zubereitung von Matcha-Tee. Sorgt für eine authentische Teezeremonie. </br> Preis: 12,99 €</p>
     <img src="img/Produkt7.jpg" alt="Produkt7">
   </div>
 
   <div class="container">
     <img src="img/Produkt8.jpg" alt="Produkt8">
-    <p class="beschreibungstext">Dies ist ein Beispieltext, der rechts neben dem Bild angezeigt wird. </p>
+    <p class="beschreibungstext">Elegante Gusseisen-Teekanne, die den Tee lange warm hält. Perfekt für eine stilvolle Teezeremonie. </br> Preis: 39,99 € (1,2 L)</p>
   </div>
 
   <div class="container">
-    <p class="beschreibungstext">Dies ist ein Beispieltext, der links neben dem Bild angezeigt wird. </p>
+    <p class="beschreibungstext">Stilvolle, doppelwandige Gläser, die den Tee warm und die Außenseite kühl halten. Ideal für den täglichen Genuss. </br> Preis: 14,99 € (2 Gläser)</p>
     <img src="img/Produkt9.jpg" alt="Produkt9">
   </div>
 
   <div class="container">
     <img src="img/Produkt12.jpg" alt="Produkt12">
-    <p class="beschreibungstext">Dies ist ein Beispieltext, der rechts neben dem Bild angezeigt wird. </p>
+    <p class="beschreibungstext">Stilvolle, luftdichte Teedosen zur Aufbewahrung von losem Tee. Im Set mit drei unterschiedlichen Designs. </br> Preis: 15,99 € (Set aus 3 Dosen)</p>
   </div>
 
 </body>
